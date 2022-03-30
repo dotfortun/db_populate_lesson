59ede7c90956c43a0d1b3a5325680827097ae09b
dotfortun <being.for.itself@gmail.com>
2022-03-30 22:25:32 +0000

    We can now populate the DB.

--- src/models.py
+++ src/models.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=False, nullable=True)
     password = db.Column(db.String(120), unique=False, nullable=True)
     title = db.Column(db.String(120), unique=False, nullable=True)
     first_name = db.Column(db.String(120), unique=False, nullable=True)
--- src/populate_db.py
+++ src/populate_db.py
@@ -4,14 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 from main import app, db
 from models import User
 
+from tqdm import tqdm
+
 users = []
 
 with open('src/users.csv', 'rt') as csv_file:
     reader = csv.DictReader(csv_file)
-    for row in reader:
-        print(row)
+    print(next(reader))
+    for row in tqdm(reader):
+        u = User(
+            email=row['email'],
+            username=row['login/username'],
+            password=row['login/password'],
+            title=row['name/title'],
+            first_name=row['name/first'],
+            last_name=row['name/last'],
+            phone=row['phone'],
+            is_active=True,
+        )
+        users.append(u)
 
 with app.app_context():
-    for user in users:
+    for user in tqdm(users):
         db.session.add(user)
     db.session.commit()
