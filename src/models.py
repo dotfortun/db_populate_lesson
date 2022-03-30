45c99d6106a689c08e85776f7682e405118c18de
dotfortun <being.for.itself@gmail.com>
2022-03-30 22:09:37 +0000

    made user model more bigger

--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -5,8 +7,15 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=True)
+    password = db.Column(db.String(120), unique=False, nullable=True)
+    title = db.Column(db.String(120), unique=False, nullable=True)
+    first_name = db.Column(db.String(120), unique=False, nullable=True)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
+    phone = db.Column(db.String(120), unique=False, nullable=True)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+    created = db.Column(db.DateTime, default=datetime.now)
+    updated = db.Column(db.DateTime, onupdate=datetime.now, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
